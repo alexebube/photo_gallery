ece42d616587ab25a1751d7da93fbc01b088acb9
Alexander Obi <alexebube@gmail.com>
2013-10-14 16:43:55 -0400

    Created a database config file in the includes folder defining the server, user, password and database name

---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+defined('DB_USER') ? null : define("DB_USER", "root");
+defined('DB_PASS') ? null : define("DB_PASS", "");
+defined('DB_NAME') ? null : define("DB_NAME", "photo_gallery");
+
+?>
