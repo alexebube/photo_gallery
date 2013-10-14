cd1e1c408079a8b8b5409dbd7cc932e841e4db7b
Alexander Obi <alexebube@gmail.com>
2013-10-14 14:01:21 -0400

    Created the functions file in the includes directory added three functions redirect_to(), output_message() and strip_zeros_from_date()

---
+++ includes/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function strip_zeros_from_date( $marked_string="" ) {
+  // first remove the marked zeros
+  $no_zeros = str_replace('*0', '', $marked_string);
+  // then remove any remaining marks
+  $cleaned_string = str_replace('*', '', $no_zeros);
+  return $cleaned_string;
+}
+
+function redirect_to( $location = NULL ) {
+  if ($location != NULL) {
+    header("Location: {$location}");
+    exit;
+  }
+}
+
+function output_message($message="") {
+  if (!empty($message)) { 
+    return "<p class=\"message\">{$message}</p>";
+  } else {
+    return "";
+  }
+}
+
+?>
