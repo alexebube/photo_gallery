2b297f5d154d6d58f60a98bf2b5dcda7fc9fbf3c
Alexander Obi <alexebube@gmail.com>
2013-10-14 17:08:13 -0400

    Created a new file database.php to handle connection to the mysql database, selection of the database and handling connection and selection database errors

---
+++ includes/database.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once ("config.php");
+
+class MySQLDatabase {
+    
+    private $connection;
+    
+    function __construct() {
+        $this->open_connection();
+    }
+    public function open_connection(){
+        $this->connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
+        if(!$this->connection){
+            die("Database connection failed: ".  mysql_error());
+        }  else {
+            $db_select = mysql_select_db(DB_NAME, $this->connection);
+            if(!$db_select){
+                die("Database Selection Failed: ".mysql_error());
+            }
+        }
+    }
+    public function close_connection(){
+        if(isset($this->connection)){
+            mysql_close($this->connection);
+            unset($this->connection);
+        }
+    }
+}
+
+$database = new MySQLDatabase();
+$db =& $database;
+?>
