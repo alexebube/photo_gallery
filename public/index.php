398efad267e3252341746870f34c0fa192328018
Alexander Obi <alexebube@gmail.com>
2013-10-15 10:51:36 -0400

    Added additional methods to the MySqlDatabase class

--- public/index.php
+++ public/index.php
@@ -4,4 +4,25 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+require_once("../includes/database.php");
+
+if (isset($database)): 
+    echo "true";
+else:
+    echo "false";
+endif;
+
+echo $database->mysql_prep("It's working well<br/>");
+
+/**
+$sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
+$sql .= "VALUES (1, 'alexebube', 'alexebube', 'Alex', 'Obi')";
+$result = $database->query($sql);  **/
+
+$sql = "SELECT * FROM users WHERE id = 1";
+$result_set = $database->query($sql);
+$found_user = $database->fetch_array($result_set);
+echo $found_user['username'];
+
 ?>
