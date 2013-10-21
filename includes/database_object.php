934417ee77c2125bb76321cd75d5b6291a01cd43
Alexander Obi <alexebube@gmail.com>
2013-10-21 12:16:44 -0400

    Created a new file database_object.php to handle common methods and attributes to all database related classes

---
+++ includes/database_object.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require_once(LIB_PATH.DS.'database.php');
+
+class DatabaseObject {
+    
+}
+?>
