c3e044b3c5f7c40483b20c6f295e50fb54c860d7
Alexander Obi <alexebube@gmail.com>
2013-10-21 09:24:50 -0400

    Modified the index.php file to include call to the __autoload function

--- public/index.php
+++ public/index.php
@@ -4,9 +4,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
+require_once ("../includes/functions.php");
 require_once("../includes/database.php");
-require_once("../includes/user.php");
+//require_once("../includes/user.php");
 
 
 $user = User::find_by_id(1);
