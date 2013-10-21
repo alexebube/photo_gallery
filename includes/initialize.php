fc5ff10f5c69740ad813ddd3c1f721e8d467f5fe
Alexander Obi <alexebube@gmail.com>
2013-10-21 10:44:24 -0400

    Ceeated a new php file initialize.php To modularize calls to require_once function And to define absolute and core paths to our files

---
+++ includes/initialize.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//define core path 
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS.'xampp'.DS.'htdocs'.DS.'photo_gallery');
+defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT.DS.'includes');
+
+require_once(LIB_PATH.DS.'config.php');
+require_once(LIB_PATH.DS.'functions.php');
+require_once(LIB_PATH.DS.'session.php');
+require_once(LIB_PATH.DS.'database.php');
+require_once(LIB_PATH.DS.'user.php');
+?>
